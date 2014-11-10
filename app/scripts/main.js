9d239111a9be0beae7a24589964bcfdec8a274a8
Patrik Vilja <patrik.vilja@gmail.com>
2014-11-10 18:07:54 +0200

    convert to es6

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,29 @@
 'use strict';
 (function () {
-  var socket = io('http://192.168.100.14:3000');
+  var socket = io('http://192.168.11.3:3000');
   var local = document.getElementById('local-video');
   var remote = document.getElementById('remote-video');
   var startButton = document.getElementById('start');
   var connection;
 
-  socket.on('offerAnswer', function (data) {
-    connection.setRemoteDescription(new RTCSessionDescription(data.sdp));
+  socket.on('offerAnswer', ({sdp}) =>  {
+    connection.setRemoteDescription(new RTCSessionDescription(sdp));
+    answer();
   });
 
-  socket.on('iceCandidate', function (data) {
-    connection.addIceCandidate(new RTCIceCandidate(data.candidate));
-  });
+  socket.on('iceCandidate', ({candidate}) => candidate && connection.addIceCandidate(new RTCIceCandidate(candidate)));
 
-  var gotDescription = function (description) {
+  var gotDescription = description => {
     connection.setLocalDescription(description);
     socket.emit('offerAnswer', {'sdp': description});
   };
-  var call = function () {connection.createOffer(gotDescription);};
-  var answer = function () {connection.createAnswer(gotDescription);};
-  var addVideoOption = function (source, select) {
-    var fragment = document.createDocumentFragment();
-    var option = document.createElement('option');
+
+  var call = () => connection.createOffer(gotDescription);
+  var answer = () => connection.createAnswer(gotDescription);
+
+  var addVideoOption = (source, select) => {
+    let fragment = document.createDocumentFragment();
+    let option = document.createElement('option');
     option.value = source.id;
     option.text = source.label || 'camera';
     fragment.appendChild(option);
@@ -30,27 +31,28 @@
   }
 
   startButton.addEventListener('click', call, false);
-  MediaStreamTrack.getSources(function (sources) {
-    var select = document.getElementById('video-select');
-    sources.forEach(function (source) {
+
+  MediaStreamTrack.getSources(sources => {
+    let select = document.getElementById('video-select');
+    sources.forEach(source => {
       if (source.kind == 'video') addVideoOption(source, select);
     });
     initialize(select);
   });
 
-  var initialize = function (select) {
+  var initialize = select => {
     connection = new RTCPeerConnection(null);
-    connection.onicecandidate = function (event) {socket.emit('offerAnswer', {'candidate': event.candidate});};
-    connection.onaddstream = function (event) {remote.src = URL.createObjectURL(event.stream);};
-    var constraints = {
+    connection.onicecandidate = ({candidate}) => socket.emit('iceCandidate', {'candidate': candidate});
+    connection.onaddstream = ({stream}) => remote.src = URL.createObjectURL(stream);
+    let constraints = {
       audio: false,
       video: { optional: [{sourceId: select.value}] }
     };
-    var success = function (stream) {
+    let success = stream => {
       local.src = URL.createObjectURL(stream);
       connection.addStream(stream);
     };
-    var failure = function () {console.log('getUserMedia failed!');};
+    let failure = () => console.log('getUserMedia failed!');
     navigator.getUserMedia(constraints, success, failure);
   };
 })();
