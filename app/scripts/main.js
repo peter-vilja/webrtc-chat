2ed1f7f92e63a90363adf2b8b110ec0d66da0bdf
Peter Vilja <peter.vilja@gmail.com>
2014-11-10 19:46:04 +0200

    add message functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,14 @@
   var local = document.getElementById('local-video');
   var remote = document.getElementById('remote-video');
   var startButton = document.getElementById('start');
-  var connection;
+  var form = document.getElementById('message-form');
+  var messageInput = document.getElementById('message-input');
+  var chat = document.getElementById('messages');
+  var connection, messages, channel;
 
   socket.on('offerAnswer', ({sdp}) =>  {
     connection.setRemoteDescription(new RTCSessionDescription(sdp));
-    answer();
+    connection.createAnswer(gotDescription);
   });
 
   socket.on('iceCandidate', ({candidate}) => candidate && connection.addIceCandidate(new RTCIceCandidate(candidate)));
@@ -18,8 +21,21 @@
     socket.emit('offerAnswer', {'sdp': description});
   };
 
-  var call = () => connection.createOffer(gotDescription);
-  var answer = () => connection.createAnswer(gotDescription);
+  var showMessage = (event) => {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(event.data));
+    chat.appendChild(li);
+  };
+
+  var createMessageChannel = (connection) => {
+    messages = connection.createDataChannel('Messages', {reliable: false});
+    messages.onmessage = showMessage;
+  };
+
+  var call = () => {
+    createMessageChannel(connection);
+    connection.createOffer(gotDescription);
+  };
 
   var addVideoOption = (source, select) => {
     let fragment = document.createDocumentFragment();
@@ -30,20 +46,15 @@
     select.appendChild(fragment);
   }
 
-  startButton.addEventListener('click', call, false);
-
-  MediaStreamTrack.getSources(sources => {
-    let select = document.getElementById('video-select');
-    sources.forEach(source => {
-      if (source.kind == 'video') addVideoOption(source, select);
-    });
-    initialize(select);
-  });
-
   var initialize = select => {
-    connection = new RTCPeerConnection(null);
+    connection = new RTCPeerConnection({iceServers: [{url: 'stun:stun.l.google.com:19302'}]}, {optional: [{RtpDataChannels: true}]});
     connection.onicecandidate = ({candidate}) => socket.emit('iceCandidate', {'candidate': candidate});
     connection.onaddstream = ({stream}) => remote.src = URL.createObjectURL(stream);
+    connection.ondatachannel = (event) => {
+      messages = event.channel;
+      messages.onmessage = showMessage;
+    };
+
     let constraints = {
       audio: false,
       video: { optional: [{sourceId: select.value}] }
@@ -55,4 +66,20 @@
     let failure = () => console.log('getUserMedia failed!');
     navigator.getUserMedia(constraints, success, failure);
   };
+
+  MediaStreamTrack.getSources(sources => {
+    let select = document.getElementById('video-select');
+    sources.forEach(source => {
+      if (source.kind == 'video') addVideoOption(source, select);
+    });
+    initialize(select);
+  });
+
+  startButton.addEventListener('click', call, false);
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    var value = messageInput.value;
+    messages.send(value);
+  });
+
 })();
